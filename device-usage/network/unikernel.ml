86fa2ab5ca99ef47e7a3a2bc256906cf32d57e53
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-02-07 04:49:01 +0000

    unix+socket: works ok with "Tcpip_stack_socket"
    Stored a stack handler in global variable to be used in other modules.
    The module type of "Tcpip_stack_socket" used here was found in
    "main.ml".
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,21 +1,36 @@
 open Lwt.Infix
 
-module Main (S: Mirage_stack_lwt.V4) = struct
+let stored_stack_handler : Tcpip_stack_socket.t option ref = ref None
+
+module MyNet = struct
+
+  module S = Tcpip_stack_socket (* from main.ml *)
+
+  let listen port =
+    match !stored_stack_handler with
+    | None -> failwith "Uninitialized"
+    | Some k ->
+      S.listen_tcpv4 k ~port (fun flow ->
+          let dst, dst_port = S.TCPV4.dst flow in
+          Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
+                        (Ipaddr.V4.to_string dst) dst_port);
+          S.TCPV4.read flow >>= function
+          | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
+          | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+          | Ok (`Data b) ->
+            Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
+            S.TCPV4.close flow
+        );
+
+      S.listen k
+
+end
+
+module Main (S: Mirage_stack_lwt.V4 with type t = Tcpip_stack_socket.t) = struct
 
   let start s =
+    stored_stack_handler := Some s; (* save in global variable *)
     let port = Key_gen.port () in
-    S.listen_tcpv4 s ~port (fun flow ->
-        let dst, dst_port = S.TCPV4.dst flow in
-        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
-                  (Ipaddr.V4.to_string dst) dst_port);
-        S.TCPV4.read flow >>= function
-        | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
-        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
-        | Ok (`Data b) ->
-          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
-          S.TCPV4.close flow
-      );
-
-    S.listen s
+    MyNet.listen port
 
 end
